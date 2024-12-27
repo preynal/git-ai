042555e79aa4f952e079b0ddaf52f2c37dfc43e2
Philippe de Reynal <1218704+preynal@users.noreply.github.com>
2024-12-27 15:09:48 +0100

    refactor: update pricing and system message in config file

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Files to exclude from diff
 export const excludedFiles = ["yarn.lock", "package-lock.json", ".env", ".env.*"];
 
-export const pricePerMillionTokens = 0.150;
+export const pricePerMillionTokens = 2.5;
 
 export const models = {
   openai: {
-    name: "gpt-4o-mini",
-    pricePerMillionTokens: 0.150
+    name: "gpt-4o",
+    pricePerMillionTokens: 2.5
   },
   anthropic: {
     name: "claude-3-sonnet-20240229",
@@ -19,7 +19,7 @@ export const selectedModel = "openai"; // Change this to switch models
 // Maximum number of tokens allowed in the diff before refusing to process
 export const maxDiffTokens = 32_768;
 
-export const systemMessage = "You are a helpful assistant that generates clear and concise git commit messages. Follow conventional commits format. The message must not exceed one line. Do not add optional scope to the commit message."
+export const systemMessage = "You are a helpful assistant that generates clear and concise git commit messages. Follow conventional commits format. The message must not exceed one line. Do not add optional scope to the commit message. eg: use feat: myfeature and never feat(scope): myfeature."
 
 export default {
   excludedFiles,
